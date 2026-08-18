a69abdac894a2cf9b1058c15b2259d2c4ec7661a
divyanshu5357 <divyanshusingh17102002@gmail.com>
2026-08-18 16:46:16 +0530

    allowed cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -52,6 +52,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:8000",
         "http://127.0.0.1:8000",
+        "https://aianalyzer-nine.vercel.app",
     ],
     allow_origin_regex=r"http://(localhost|127\.0\.0\.1)(:[0-9]+)?",
     allow_credentials=True,
